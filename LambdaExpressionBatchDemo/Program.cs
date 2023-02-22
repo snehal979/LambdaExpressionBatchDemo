43b9a53f2e142c67a94e7864be97f4bcf2db3610
Snehal979 <bansodsnehal535@gmail.com>
2023-02-23 00:13:33 +0530

    [Snehal]Add:Welcome message

---
+++ LambdaExpressionBatchDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace LambdaExpressionBatchDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Lambda Expression Batch Demo");
+            Console.ReadLine();
+        }
+
+    }
+}
